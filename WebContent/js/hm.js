b70a94542f805116614d36577883f831a96fbdfe
Hasan MEN <hmen.56@gmail.com>
2016-07-13 15:10:06 +0300

    update test

--- WebContent/js/hm.js
+++ WebContent/js/hm.js
@@ -13,6 +13,7 @@ function getPerson(){
 		  success : function(retVal){			
 			var mydata = [{tc:retVal.tc,name:retVal.name,phone:retVal.phone}];
 				
+			/* update test */
 				$("#gridTable").jqGrid({
 					datatype: "local",
 					height: 100,
